9d7cf9f0b4b4c6d2d26853dce7f82161ab05cd4b
Raghuram Sundararajan <raghurams@iisc.ac.in>
2023-01-17 02:23:19 +0530

    Notation, style changes, and a `CoeFun` instance.

--- RealsQuasiMorphisms/Basic.lean
+++ RealsQuasiMorphisms/Basic.lean
@@ -1,10 +1,16 @@
 import Mathlib.Data.Int.AbsoluteValue
 import Mathlib.Tactic.Linarith
 
+-- For convenience. We can think about scoping this with sections later.
+local notation (priority := high) "|" x "|" => Int.natAbs x
+
 variable {G : Type u} [AddCommGroup G]
 
-variable (G) in structure quasiHomomorphism where 
-  func : G → ℤ
+variable (G) in
+structure QuasiMorphism where
+  toFun : G → ℤ
   bound : ℕ 
-  almostAdditive x y : (func (x+y) - func x - func y).natAbs ≤ bound
+  almostAdditive x y : |toFun (x + y) - toFun x - toFun y| ≤ bound
 
+instance : CoeFun (QuasiMorphism G) fun _ => G → ℤ where
+  coe := QuasiMorphism.toFun
