8c7ee47a8ea67f4f8100a224e353bebd3dfe2725
Raghuram Sundararajan <raghurams@iisc.ac.in>
2023-02-02 22:33:50 +0530

    Add documentation to lemmas.

--- RealsQuasiMorphisms/Basic.lean
+++ RealsQuasiMorphisms/Basic.lean
@@ -2,6 +2,12 @@ import Mathlib.Data.Int.AbsoluteValue
 import Mathlib.Tactic.Linarith
 import Mathlib.Tactic.LibrarySearch
 
+/-! Defines quasi-morphisms from an abelian group to ℤ and algebraic operations on them.
+
+Reference(s):
+1. http://web.science.mq.edu.au/~street/EffR.pdf
+-/
+
 -- Note: we can avoid the AbsoluteValue import by using simp? to get
 -- exact `simp only`s for every use. However, this results in huge lists
 -- of lemmas sometimes, so this hasn't been done for now.
@@ -25,11 +31,13 @@ namespace QuasiMorphism
 section AlmostProperties
 variable (f : QuasiMorphism G) (g : G) (m : ℤ)
 
+/-- A quasi-morphism `f` maps 0 to 0, within an error of up to `f.bound`. -/
 lemma almost_zero : |f 0| ≤ f.bound := by simpa using f.almostAdditive 0 0
 /-   calc |f 0| = | -(f 0)|             := by rw [Int.natAbs_neg]
            _ = |f (0+0) - f 0 - f 0| := congrArg (|·|) <| by abel_nf
            _ ≤ f.bound               := f.almostAdditive 0 0 -/
 
+/-- A quasi-morphism `f` respects negation, within an error of up to `f.bound * 2`. -/
 lemma almost_neg : |f (-g) - (- (f g))| ≤ f.bound * 2 :=
   calc |f (-g) - (- (f g))|
            = |(f (-g) + f g - f 0) + f 0|
@@ -44,9 +52,9 @@ lemma almost_neg : |f (-g) - (- (f g))| ≤ f.bound * 2 :=
                := Nat.mul_two .. ▸ Nat.add_le_add (f.almostAdditive (-g) g)
                                                   f.almost_zero
 
-/-- Reference: first inequality proven in
-http://web.science.mq.edu.au/~street/EffR.pdf. -/
-private lemma almost_smul : |f (m • g) - m * f g| ≤ f.bound * (|m| + 1) := by
+/- First inequality proven in reference 1. -/
+/-- A quasi-morphism `f` respects scaling by ℤ, within an error proportional to the scaling factor. -/
+lemma almost_smul : |f (m • g) - m * f g| ≤ f.bound * (|m| + 1) := by
   cases m <;> (rename_i m; induction m)
   case ofNat.zero => simp; exact f.almost_zero
   case ofNat.succ m hᵢ =>
@@ -86,8 +94,8 @@ private lemma almost_smul : |f (m • g) - m * f g| ≤ f.bound * (|m| + 1) := b
       _ = f.bound * (|Int.negSucc m.succ| + 1)
             := by simp only [Int.natAbs_negSucc]; linarith
 
-/-- Reference: second inequality proven in
-http://web.science.mq.edu.au/~street/EffR.pdf. -/
+/- Second inequality proven in reference 1, generalised to arbitrary abelian groups. -/
+/-- A kind of commutativity of scaling by ℤ, one scale factor before and another after applying a quasi-morphism. -/
 private lemma almost_smul_comm (m n : ℤ)
     : |n * f (m • g) - m * f (n • g)| ≤ f.bound * (|m| + |n| + 2) :=
   calc |n * f (m • g) - m * f (n • g)|
@@ -104,7 +112,7 @@ private lemma almost_smul_comm (m n : ℤ)
     _ = f.bound * (|m| + |n| + 2) := by linarith
 
 /- `almost_smul_comm'` specialised to quasi-morphisms on integers and applied to 1.
-Eq (1) of http://web.science.mq.edu.au/~street/EffR.pdf. -/
+Eq (1) of reference 1. -/
 private lemma almost_smul_comm' (f : QuasiMorphism ℤ) (m n : ℤ)
     : |n * f m - m * f n| ≤ f.bound * (|m| + |n| + 2) := by
   conv => lhs; rewrite [←congrArg f (zsmul_int_one m), ←congrArg f (zsmul_int_one n)]
@@ -112,6 +120,7 @@ private lemma almost_smul_comm' (f : QuasiMorphism ℤ) (m n : ℤ)
 
 end AlmostProperties
 
+/-- Composition of quasi-morphisms on ℤ, returning another quasi-morphism. -/
 def comp  (f : QuasiMorphism ℤ) (g : QuasiMorphism ℤ) : QuasiMorphism ℤ where
   toFun := f ∘ g
   bound := sorry
@@ -136,6 +145,7 @@ def comp  (f : QuasiMorphism ℤ) (g : QuasiMorphism ℤ) : QuasiMorphism ℤ wh
 instance : HAdd (QuasiMorphism G) (QuasiMorphism G) (QuasiMorphism G) where
   hAdd := sorry
 
+/-- Addition of quasi-morphisms. -/
 def add (f g : QuasiMorphism G): QuasiMorphism G where 
   toFun := f + g
   bound := f.bound + g.bound
