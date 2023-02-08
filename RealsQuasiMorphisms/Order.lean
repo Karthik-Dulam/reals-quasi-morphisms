94c29465112d73d04e7326013184bab6bc56732b
Lord-of-the-Galaxy <25320402+Lord-of-the-Galaxy@users.noreply.github.com>
2023-02-08 22:35:04 +0530

    Currently useless

--- RealsQuasiMorphisms/Order.lean
+++ RealsQuasiMorphisms/Order.lean
@@ -1,7 +1,38 @@
 import RealsQuasiMorphisms.Basic
 import Mathlib.Algebra.Order.Field.Defs
 import RealsQuasiMorphisms.Algebra
+
+namespace AlmostHom
+
+variable {G : Type} [OrderedAddCommGroup G]
+
+def is_pos (f : AlmostHom G) : Prop := ∃ a : ℤ , ∀ x : G, x ≥ 0 → f x ≥ a
+
+def is_neg (f : AlmostHom G) : Prop := ∃ b : ℤ , ∀ x : G, x ≥ 0 → f x ≥ b
+
+def le (f g : AlmostHom G) : Prop := is_pos (g - f)
+
+instance : Preorder (AlmostHom G) where
+  le := le
+  le_refl f := by
+                simp only [le, is_pos, sub_self]
+                use -1; intro x hx
+                show -1 ≤ 0; simp only [Left.neg_nonpos_iff]
+  le_trans p q r:= by
+                    intro hpq hqr
+                    simp only [le, is_pos] at hpq hqr ⊢
+                    let ⟨a, hpq⟩ := hpq; let ⟨b, hqr⟩ := hqr
+                    use a+b; intro x hx
+                    sorry
+  
+
+-- instance : OrderedAddCommGroup (AlmostHom G) where
+--   add_le_add_left := sorry
+
+end AlmostHom
+
 namespace QuasiHom
+
 
 instance : LinearOrderedField (QuasiHom ℤ) where
   le := sorry
